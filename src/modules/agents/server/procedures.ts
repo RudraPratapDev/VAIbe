4c5b22cd5246f7957f36cf208d9a101f2e49bea8
RudraPratapDev <rudra.tomar608@gmail.com>
2025-08-29 12:13:02 +0530

    feat(agents):implement complete agent detail view with loading states

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -6,6 +6,8 @@ import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, PAGE_MAX_SIZE, PAGE_MIN_SIZE } from "@/constants";
+import { trpc } from "@/trpc/server";
+import { TRPCError } from "@trpc/server";
 
 
 export const agentsRouter=createTRPCRouter({
@@ -56,14 +58,21 @@ export const agentsRouter=createTRPCRouter({
 
     }),
 
-    getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input})=>{
+    getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input,ctx})=>{
         const [existingAgent]=await db
         .select({
             meetingCount:sql<number>`5`,
             ...getTableColumns(agents),
         })
         .from(agents)
-        .where(eq(agents.id,input.id));
+        .where(
+            and(
+                eq(agents.id,input.id),
+                eq(agents.userId,ctx.auth.user.id)
+            )
+        );
+
+        if(!existingAgent) throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
 
         return existingAgent;
     }),
---
+++ src/modules/agents/ui/components/agent-id-view-header.tsx
@@ -0,0 +1,55 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { ChevronRightIcon, MoreVerticalIcon, PencilIcon, TrashIcon } from "lucide-react";
+import Link from "next/link";
+import { Chevron } from "react-day-picker";
+
+interface Props {
+    agentId: string;
+    agentName: string | undefined;
+    onEdit: () => void;
+    onRemove: () => void;
+}
+
+export const AgentIdViewHeader = ({ agentId, agentName, onEdit, onRemove }: Props) => {
+    return (
+        <div className="flex items-center justify-between">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild className="font-medium text-xl">
+                            <Link href="/agents">My Agents</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className=" text-foreground text-xl font-medium [&>svg]:w-4 [&>svg]:h-4">
+                        <ChevronRightIcon />
+                    </BreadcrumbSeparator>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild className="font-medium text-xl text-foreground/70">
+                            <Link href={`/agents/${agentId}`}>{agentName}</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" >
+                        <MoreVerticalIcon />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-40 ">
+                    <div className="flex items-center gap-2 hover:text-blue-500 hover:cursor-pointer" onClick={onEdit}>
+                        <PencilIcon className="size-4  hover:text-blue-500 hover:cursor-pointer" />
+                        <span>Edit</span>
+                    </div>
+                    <div className="flex items-center gap-2 hover:text-red-500 hover:cursor-pointer" onClick={onRemove}>
+                        <TrashIcon className="size-4  hover:text-red-500 hover:cursor-pointer" />
+                        <span>Delete</span>
+                    </div>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
+};
